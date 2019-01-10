55680abcce19af9664851a60ecfd6481a41b149d
Yifu Yan <yanyifu94@hotmail.com>
2019-01-09 23:42:24 -0800

    ran RMD check and fixed some issues

--- R/timer_function.R
+++ R/timer_function.R
@@ -5,7 +5,7 @@
 #' @return a timer object.
 #' @examples
 #' timer1 <- createTimer() # print is enabled
-#' timer1 <- createTimer(F) # print is disabled
+#' timer1 <- createTimer(FALSE) # print is disabled
 #' timer1$start("event1") # start timing for event 1
 #' timer1$stop("event1", comment = "event 1 stopped") # stop timing for event 1(comment is optional)
 #' getTimer(timer1) # get all records in a data frame
@@ -23,11 +23,11 @@ createTimer <- function(verbose = T){
 #' @param object The name for timer object.
 #' @return A data frame containing all records of a timer object.
 #' @examples
-#' timer1 <- createtimer()
+#' timer1 <- createTimer()
 #' timer1$start("event1")
 #' Sys.sleep(1)
 #' timer1$stop("event1")
-#' gettimer(timer1)
+#' getTimer(timer1)
 #' @export
 getTimer <- function(object){
     stopifnot(any(class(object) == "timer"))
